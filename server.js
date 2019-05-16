62348bb45272cc96d817de81f3ec17f35da35005
Glitch (cerulean-catshark) <none>
2019-05-16 19:14:22 +0000

    👾⏳ Updated with Glitch

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+const bcrypt = require('bcrypt');
 
 fccTesting(app); //For FCC testing purposes
 
@@ -10,17 +11,18 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err,hash)=>{
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => { 
+    console.log(res);
+  });
+});
 //END_ASYNC
 
 //START_SYNC
-
-
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 //END_SYNC
 
 
